0ec8abfee09e118587357f357c4af1e89fff1084
SagarBoyal <sboyal12@gmail.com>
2023-01-13 20:51:13 +0530

    Updated Handling Exception and Generic class

--- DAY_7/src/Stack.java
+++ DAY_7/src/Stack.java
@@ -1,27 +1,33 @@
-public class Stack {
+public class Stack <E>{
+
     private int top;
-    private int stackArray[];
-    public Stack(){
-        top=-1;
-        stackArray = new int[5];
+    private E array[];
+    public Stack() {
+        array = (E[]) new Object[5];
+        top = -1;
     }
-    public Stack(int n){
-        top=-1;
-        stackArray = new int[n];
+
+    public Stack(int n) {
+        if(n<0) throw new InvalidStackSizeException();
+        array = (E[]) new Object[n];
+        top =-1;
     }
-    public void push(int ele){
-        stackArray[++top] = ele;
+    public void push(E data)throws StackOverFlowException {
+        if(top==array.length-1)throw new StackOverFlowException();
+        array[++top]=data;
     }
-    public int pop(){
-        if(top<0) System.out.println("Stack UnderFlow");;
-        int data = stackArray[top];
+
+    public E pop() throws  StackUnderFlowException{
+        if(top<0) throw new StackUnderFlowException();
+        E data = array[top];
         top--;
         return data;
     }
     public void display(){
         if(top==-1){System.out.println("Stack is Empty!");return;}
         System.out.print("Elements are: ");
-        for(int i=0;i<=top;i++) System.out.print(stackArray[i]+" ");
+        for(int i=0;i<=top;i++) System.out.print(array[i]+" ");
         System.out.println();
     }
+
 }
