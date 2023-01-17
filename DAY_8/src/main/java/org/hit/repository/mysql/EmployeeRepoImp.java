a4c7e38a13b7e64975ebece4e29d1730d0616af6
SagarBoyal <sboyal12@gmail.com>
2023-01-17 18:57:45 +0530

    jdbc implemented

--- DAY_8/src/main/java/org/hit/repository/mysql/EmployeeRepoImp.java
+++ DAY_8/src/main/java/org/hit/repository/mysql/EmployeeRepoImp.java
@@ -3,10 +3,8 @@ package org.hit.repository.mysql;
 import org.hit.model.Employee;
 import org.hit.repository.EmployeeRepo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeRepoImp implements EmployeeRepo {
@@ -28,14 +26,15 @@ public class EmployeeRepoImp implements EmployeeRepo {
 
     @Override
     public Employee addEmployee(Employee e) {
-        String query ="INSERT INTO employee VALUES(?,?,?)";
+        Employee emp = findEmployee(e.getEid());
         try {
-            PreparedStatement ps = connection.prepareStatement(query);
+            if(emp!=null)return null;
+            PreparedStatement ps = connection.prepareStatement("INSERT INTO employee VALUES(?,?,?)");
             ps.setInt(1,e.getEid());
             ps.setString(2,e.getEname());
             ps.setFloat(3,e.getSalary());
-            ps.executeUpdate();
-            if(ps!=null) return e;
+            int count = ps.executeUpdate();
+            if(count>0)return e;
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
@@ -44,21 +43,58 @@ public class EmployeeRepoImp implements EmployeeRepo {
 
     @Override
     public Employee updateEmployee(Employee e) {
+        Employee emp = findEmployee(e.getEid());
+        try {
+            if(emp==null)return null;
+            PreparedStatement ps = connection.prepareStatement("UPDATE employee SET employee_name=?, employee_salary=? WHERE employee_id=?");
+            ps.setInt(3,e.getEid());
+            ps.setString(1,e.getEname());
+            ps.setFloat(2,e.getSalary());
+            int count = ps.executeUpdate();
+            if(count>0)return e;
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
         return null;
     }
 
     @Override
     public Employee deleteEmployee(Integer eid) {
+        Employee emp = findEmployee(eid);
+        try {
+            if(emp==null)return null;
+            PreparedStatement ps = connection.prepareStatement("DELETE FROM employee WHERE employee_id=?");
+            int count = ps.executeUpdate();
+            if(count>0)return emp;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
     public Employee findEmployee(Integer eid) {
+        try {
+            PreparedStatement ps = connection.prepareStatement("SELECT * FROM employee where employee_id=?");
+            ps.setInt(1,eid);
+            ResultSet resultSet = ps.executeQuery();
+            if(resultSet.next())return new Employee(resultSet.getInt(1),resultSet.getString(2),resultSet.getFloat(3));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
     @Override
     public List<Employee> findAllEmployee() {
-        return null;
+        List<Employee> employeeList = new ArrayList<>();
+        try {
+            Statement st = connection.createStatement();
+            ResultSet resultSet = st.executeQuery("SELECT * FROM employee");
+            while (resultSet.next())employeeList.add(new Employee(resultSet.getInt(1),resultSet.getString(2),resultSet.getFloat(3)));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return employeeList;
     }
 }
